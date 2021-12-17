8f357788b3a49733451195e52d9bac9a89682d60
matheus3i <matheusf.conde@gmail.com>
2021-12-17 16:29:49 -0300

    OCP violacao

---
+++ SOLID/2- OCP/OCP.Violacao/DebitoConta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.OCP.Violacao
+{
+    public class DebitoConta
+    {
+        public void Debitar (decimal valor, string conta, TipoConta tipoConta)
+        {
+            if(tipoConta == TipoConta.Corrente)
+            {
+                //Debita Conta Corrente
+                //implementação não vem ao caso, quero apenas demonstrar
+                //o conhecimento em Open Closed Principle.
+            }
+
+            if (tipoConta != TipoConta.Poupanca)
+            {
+                //Valida Aniversário da Conta
+                //Debita Conta Poupança
+                //implementação não vem ao caso, quero apenas demonstrar
+                //o conhecimento em Open Closed Principle.
+            }
+        }
+    }
+}
