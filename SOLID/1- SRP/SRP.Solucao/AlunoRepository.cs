2decf24fc89aa1740e09952d7d16099aaafb8200
matheus3i <matheusf.conde@gmail.com>
2021-12-17 15:36:29 -0300

    SRP

---
+++ SOLID/1- SRP/SRP.Solucao/AlunoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+
+namespace SOLID.SRP.Solucao
+{
+    public class AlunoRepository
+    {
+        public void AdicionarAluno(Aluno aluno)
+        {
+            //implementação não vem ao caso, quero apenas demonstrar
+            //o conhecimento em Single Responsability Principle.
+            //using (var cn = new SqlConnection())
+            //{
+                
+            //}
+        }
+    }
+}
---
+++ SOLID/1- SRP/SRP.Solucao/CPF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.SRP.Solucao
+{
+    public class CPF
+    {
+        public string Numero { get; set; }
+
+        public bool Validar()
+        {
+            return Numero.Length == 11;
+        }
+
+        public void AdicionaCPF(string cpf)
+        {
+            this.Numero = cpf;
+        }
+    }
+}
