44b01c470573b789684a18ccf74d2e3aa5c3dad3
matheus3i <matheusf.conde@gmail.com>
2021-12-29 15:09:09 -0300

    Interface Segregation Principle

---
+++ SOLID/4- ISP/ISP.Solucao/CadastroProduto.cs
@@ -0,0 +1,22 @@
+﻿using SOLID.ISP.Solucao.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.ISP.Solucao
+{
+    public class CadastroProduto : ICadastroProduto
+    {
+        public void SalvarBanco()
+        {
+            //Insert na tabela Produto
+        }
+
+        public void ValidarDados()
+        {
+            //Validar Valor
+        }
+    }
+}
---
+++ SOLID/4- ISP/ISP.Violacao/ICadastro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.ISP.Violacao
+{
+    public interface ICadastro
+    {
+        void ValidarDados();
+        void SalvarBanco();
+        void EnviarEmail();
+    }
+}
